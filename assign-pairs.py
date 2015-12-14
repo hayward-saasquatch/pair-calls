53c976ab4fecbc1ba280d697bc95a4a4a280e89c
Ian Ross <iross@cadasta.org>
2015-12-14 18:10:31 +0100

    Fix call assignment script

--- assign-pairs.py
+++ assign-pairs.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-import os, os.path
+import os
+import os.path
+import random
+import itertools
 from datetime import date, datetime, timedelta
 
 
@@ -9,14 +12,22 @@ from datetime import date, datetime, timedelta
 
 class Pair:
     def __init__(self, n1, n2): self.p = (min(n1, n2), max(n1, n2))
-    def __getitem__(self, i):   return self.p[i]
-    def __eq__(self, other):    return self.p == other.p
-    def __hash__(self):         return self.p.__hash__()
-    def __repr__(self):         return '({}, {})'.format(self.p[0], self.p[1])
+
+    def __getitem__(self, i): return self.p[i]
+
+    def __eq__(self, other): return self.p == other.p
+
+    def __hash__(self): return self.p.__hash__()
+
+    def __repr__(self): return '({}, {})'.format(self.p[0], self.p[1])
+
     def other(self, n):
-        if   self.p[0] == n: return self.p[1]
-        elif self.p[1] == n: return self.p[0]
-        else:                return None
+        if self.p[0] == n:
+            return self.p[1]
+        elif self.p[1] == n:
+            return self.p[0]
+        else:
+            return None
 
 
 # Find the next Monday after the last week already set up (or start
@@ -30,14 +41,14 @@ def next_week():
     else:
         monday = date.today()
         if monday.weekday() != 0:
-            monday += timedelta(days = 7 - monday.weekday())
+            monday += timedelta(days=7-monday.weekday())
     return monday
 
 
 # Read "latest call" map between pairs and dates.
 
 def read_latest(names):
-    latest = { }
+    latest = {}
     try:
         for l in open('latest.txt'):
             n1, n2, d = l.split(',')
@@ -47,7 +58,8 @@ def read_latest(names):
         d = date(1970, 1, 1)
         for n1 in names:
             for n2 in names:
-                if n1 != n2: latest[Pair(n1, n2)] = d
+                if n1 != n2:
+                    latest[Pair(n1, n2)] = d
     return latest
 
 
@@ -64,27 +76,50 @@ def write_latest(latest):
 # between a pair.
 
 def assign_pairs(names, latest, day):
-    unassigned = set(names)
+    # Make list of all possible pairs.
+    all_pairs = list(set([Pair(n1, n2) for n1 in names for n2 in names
+                          if n1 != n2]))
+    npairs = len(all_pairs)
+
+    # Add default last meeting times and random tiebreakers.
+    tiebreakers = list(range(0, npairs))
+    random.seed()
+    random.shuffle(tiebreakers)
+    keys = zip(itertools.repeat(date(1970, 1, 1)), tiebreakers)
+    all_pairs = dict(zip(all_pairs, keys))
+
+    # Set last meeting times.
+    for k, v in latest.items():
+        all_pairs[k] = (v, all_pairs[k][1])
+
+    # Sort by meeting times and tiebreakers.
+    work_pairs = sorted(all_pairs.keys(), key=lambda k: all_pairs[k],
+                        reverse=True)
+    left_pairs = work_pairs
+
+    # Pull out oldest pairs.
     pairs = []
-    while len(unassigned) > 0:
-        #print('unassigned =', unassigned)
-        n1 = unassigned.pop()
-        #print('n1 =', n1)
-        cmap = { k:v for k, v in latest.items()
-                 if n1 in k and k.other(n1) in unassigned }
-        #print('cmap =', cmap)
-        cpairs = sorted(cmap, key=cmap.get)
-        #print('cpairs =', cpairs)
-        if len(cpairs) > 0:       n2 = cpairs[0].other(n1)
-        elif len(unassigned) > 0: n2 = unassigned.pop()
-        else:
-            others = set(names)
-            others.remove(n1)
-            n2 = others.pop()
-        #print('n2 =', n2)
-        if n2 in unassigned: unassigned.remove(n2)
-        pairs.append(Pair(n1, n2))
-        latest[Pair(n1, n2)] = day
+    while len(work_pairs) > 0:
+        print(work_pairs)
+        pair = work_pairs.pop()
+        work_pairs = [p for p in work_pairs if pair[0] not in p]
+        work_pairs = [p for p in work_pairs if pair[1] not in p]
+        pairs.append(pair)
+        latest[pair] = day
+
+    # If there is an odd number of people, we'll be missing someone.
+    # Work out who.
+    left_name = names
+    for i in range(len(pairs)):
+        left_name = [n for n in left_name if n not in pairs[i]]
+    left_name = left_name[0]
+
+    # Get the oldest pair for the missing person.
+    left_pairs = [p for p in left_pairs if left_name in p]
+    pair = left_pairs.pop()
+    pairs.append(pair)
+    latest[pair] = day
+
     return pairs
 
 
